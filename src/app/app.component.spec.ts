4eb823ecf02d87cb0c21309faaf21eff7ca8e328
James Riall <jriall@google.com>
2019-04-06 10:56:26 +0100

    Clean up Angular CLI generated elements

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,17 +19,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'example-testing-application'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('example-testing-application');
-  });
-
-  it('should render title in a h1 tag', () => {
+  it('should render the router outlet', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent)
-        .toContain('Welcome to example-testing-application!');
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
   });
 });
