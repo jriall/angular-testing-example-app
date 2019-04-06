984f72951953a9a6a0e0e56301be47811b4782d0
James Riall <jriall@google.com>
2019-04-06 15:00:38 +0100

    Add film list resolver test

---
+++ src/app/film-list-resolver.spec.ts
@@ -0,0 +1,37 @@
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {async, TestBed} from '@angular/core/testing';
+import {of as observableOf} from 'rxjs';
+
+import {DataService} from './data-service';
+import {FilmListResolver} from './film-list-resolver';
+
+const MOCK_FILM_LIST = [];
+
+describe('The film list resolver', () => {
+  let filmListResolver: FilmListResolver;
+  let filmListSpy: jasmine.Spy;
+
+  beforeEach(async(() => {
+    TestBed
+        .configureTestingModule({
+          imports: [HttpClientTestingModule],
+          providers: [FilmListResolver],
+        })
+        .compileComponents();
+
+    filmListSpy = spyOn(TestBed.get(DataService), 'getFilmList')
+                      .and.returnValue(observableOf(MOCK_FILM_LIST));
+    filmListResolver = TestBed.get(FilmListResolver);
+  }));
+
+  it('calls the getFilmList method on the data service', () => {
+    filmListResolver.resolve();
+    expect(filmListSpy).toHaveBeenCalled();
+  });
+
+  it('returns an observable of the filmList', () => {
+    filmListResolver.resolve().subscribe(response => {
+      expect(response).toEqual(MOCK_FILM_LIST);
+    });
+  });
+});
